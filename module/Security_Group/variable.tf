977d5ae76a6b6ce64ca971240c29ef13a5682a89
adwordshin <136399595+adwordshin@users.noreply.github.com>
2023-07-24 08:49:34 +0900

    Update variable.tf

--- module/Security_Group/variable.tf
+++ module/Security_Group/variable.tf
@@ -1,19 +1,19 @@
 ### Security Group Variable
 variable "vpc-name" {
-  type = "string"
+  type = string
   default = "seoul-center"
   description = "data로 가져올 VPC 이름 정의"
 }
 
 variable "bastion-sg-name" {
-  type = "string"
+  type = string
   default = "seoul-center-bastion-sg"
   description = "Bastion Host Security Group 이름 정의"
 }
 
 
 variable "eks-cluster-sg-name" {
-  type = "string"
+  type = string
   default = "seoul-eks-cluster-sg"
   description = "seoul-eks-cluster Security Group 이름 정의"
 }
