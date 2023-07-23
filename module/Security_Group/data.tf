6ea143fcd7dbadd2f813f136b6f00b52e732c227
adwordshin <136399595+adwordshin@users.noreply.github.com>
2023-07-24 08:38:44 +0900

    Add files via upload

---
+++ module/Security_Group/data.tf
@@ -0,0 +1,6 @@
+data "aws_vpc" "VPC" {
+  filter {
+    name = "tag:Name"
+    values = var.vpc-name
+  }
+}
---
+++ module/Security_Group/main.tf
@@ -0,0 +1,66 @@
+resource "aws_security_group" "create-bastion-sg" {
+  vpc_id = data.aws_vpc.VPC.id
+  name   = "create-bastion-sg"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.bastion-sg-name
+  }
+}
+
+resource "aws_security_group" "create-eks-cluster-sg" {
+  vpc_id = data.aws_vpc.VPC.id
+  name   = "create-eks-cluster-sg"
+
+  ingress {
+    from_port   = 443
+    to_port     = 8443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.eks-cluster-sg-name
+  }
+}
