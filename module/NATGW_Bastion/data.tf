8d7eae5d000ebc85dce84482916cee167f1da460
adwordshin <136399595+adwordshin@users.noreply.github.com>
2023-07-24 10:12:03 +0900

    Add files via upload

---
+++ module/NATGW_Bastion/data.tf
@@ -0,0 +1,20 @@
+data "aws_vpc" "VPC" {
+  filter {
+    name = "tag:Name"
+    values = [var.vpc-name]
+  }
+}
+
+data "aws_subnet" "pub-2a" {
+  filter {
+    name = "tag:Name"
+    values = [var.pub-2a-name]
+  }
+}
+
+data "aws_route_table" "pvt-rtb" {
+  filter {
+    name = "tag:Name"
+    values = [var.pvt-rtb-name]
+  }
+}
