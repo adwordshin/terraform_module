27dff20b9f57c7682a22407d689c0a63250ea641
adwordshin <136399595+adwordshin@users.noreply.github.com>
2023-07-24 16:14:12 +0900

    Add files via upload

---
+++ module/Cluster/data.tf
@@ -0,0 +1,26 @@
+data "aws_iam_role" "cluster-role" {
+  filter {
+    name = "tag:Name"
+    values = [var.cluster-role-name]
+}
+
+data "aws_security_group" "cluster-sg" {
+  filter {
+    name = "tag:Name"
+    values = [var.cluster-sg-name]
+  }
+}
+
+data "aws_subnet" "pvt-2a-name" {
+  filter {
+    name = "tag:Name"
+    values = [var.pvt-2a-name]
+  }
+}
+
+data "aws_subnet" "pvt-2c-name" {
+  filter {
+    name = "tag:Name"
+    values = [var.pvt-2c-name]
+  }
+}
